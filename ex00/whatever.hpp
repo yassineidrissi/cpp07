190d98e2acbce6d1c89fbf35f16371aa567b7678
yassine idrissi <yassine42idrissi@gmail.com>
2024-01-03 11:36:23 +0100

    finish ex00

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: yassine <yassine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 20:50:02 by yassine           #+#    #+#             */
-/*   Updated: 2024/01/02 20:50:20 by yassine          ###   ########.fr       */
+/*   Updated: 2024/01/03 11:35:02 by yassine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,25 @@
 
 #include <iostream>
 
+template <typename T>
+
+void    swap(T &a, T &b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+
+T       min(T &a, T &b)
+{
+    return (a < b ? a : b);
+}
+
+template <typename T>
+
+T       max(T &a, T &b)
+{
+    return (a > b ? a : b);
+}
