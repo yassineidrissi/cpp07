f2689122eeaa4e446809521ce8a0a090c38d3e2f
Yassine Idrissi <yaidriss@c2r3p4.1337.ma>
2024-01-03 18:23:54 +0100

    add ex02

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -3,14 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yassine <yassine@student.42.fr>            +#+  +:+       +#+        */
+/*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 20:50:02 by yassine           #+#    #+#             */
-/*   Updated: 2024/01/02 20:50:20 by yassine          ###   ########.fr       */
+/*   Updated: 2024/01/03 18:03:01 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <cctype>
 
+template <typename T>
+
+void swap(T &a, T &b)
+{
+	T tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+
+T &min(T &a, T &b)
+{
+	if (a < b)
+		return a;
+	else
+		return b;
+}
+
+template <typename T>
+
+T &max(T &a, T &b)
+{
+	if (a < b)
+		return b;
+	else
+		return a;
+}
