bc0c14c9bb6c11fd23d02f414f215c0d6a38bcac
Yassine Idrissi <yaidriss@c2r1p9.1337.ma>
2024-01-05 00:54:04 +0100

    all modifications

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 18:34:53 by yaidriss          #+#    #+#             */
-/*   Updated: 2024/01/03 21:22:00 by yaidriss         ###   ########.fr       */
+/*   Updated: 2024/01/05 00:52:04 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 #include <iostream>
 
+template <typename T>
 class Array
 {
-	
+	public:
+		Array();
+		~Array();
+		Array(unsigned int n);
+		Array(const other);
+		
 }
