c0de017f4618740a8f1ff32b42b7caa05e2c2666
Yassine Idrissi <yaidriss@c2r1p9.1337.ma>
2024-01-05 00:54:54 +0100

    add signiture

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 18:34:53 by yaidriss          #+#    #+#             */
-/*   Updated: 2024/01/05 00:52:04 by yaidriss         ###   ########.fr       */
+/*   Updated: 2024/01/05 00:54:35 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,26 @@ class Array
 		Array(const other);
 		
 }
+
+
+// #pragma once
+
+// #include <iostream>
+
+// template <typename T>
+// class Array
+// {
+//     public:
+//         Array();
+//         ~Array();
+//         Array(unsigned int n);
+//         Array(const Array<T>& other);
+//         Array& operator=(const Array<T>& other);
+//         T& operator[](unsigned int index);
+
+//     private:
+//         T* _array;
+//         unsigned int _size;
+// };
+
+// #include "Array.tpp"
